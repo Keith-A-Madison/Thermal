3cb66d1b2d85393186d423a4f1dda4496328f29b
Keith Madison <96896521+Keith-A-Madison@users.noreply.github.com>
2023-07-19 18:08:01 -0400

    Update montecarlo.cpp

--- src/montecarlo.cpp
+++ src/montecarlo.cpp
@@ -1,43 +1,80 @@
 #include <boost/numeric/odeint.hpp>
 #include "montecarlo.h"
 
-using namespace boost::numeric::odeint;
+void MonteCarlo::takeStep(const stateType& state, stateType& ds,
+			  double& path, double& loss, double& intensity){
 
-MonteCarlo::MonteCarlo(): gen(rd()){}
+	// Compute step due to refraction
+	std::array<double, 3> position = {state[3], state[4], state[5]};
+	std::array<double, 3> grad = index(position);
 
-double MonteCarlo::uniformRand(){
+	path += std::sqrt(pow(state[3], 2), pow(state[4], 2), pow(state[5], 2));
 
-	return dist(gen);
+	ds = {state[3], state[4], state[5], grad[0], grad[1], grad[2]};
+
+	// Compute step due to attenuation
+	if(dist(gen) < std::exp(path / atten(position))){
+		// Phase updates ds, loss by reference
+		phase(ds, loss);
+		path = 0;
+	}
 
 }
 
-void MonteCarlo::stochStep(const stateType& state, stateType& ds, const double /*t*/){
+double MonteCarlo::traceRay(const stateType& init){
 
-	ds[0] = state[3]; ds[3] = 0;
-	ds[1] = state[4]; ds[4] = 0;
-	ds[2] = state[5]; ds[5] = -0.006832 * exp(0.016 * ds[2]);
+	using stepperType = boost::numeric::odeint::runge_kutta_dopri5<stateType>;
 
-	// The interaction probability is dependent upon this step size
-	double stepSize = sqrt(pow(ds[0] - prevState[0], 2) + 
-			       pow(ds[1] - prevState[1], 2) + 
-			       pow(ds[2] - prevState[2], 2));
+	auto observer = [&](const stateType& state, double t){};
 
-	if(uniformRand() < (1 - exp(-n * sigma * stepSize))
-		// Interaction occurs, compute new direction, intensity
-}
+	double path = 0, loss = 1, intensity = 0;
 
-void MonteCarlo::rayTrace(const stateType& initState){
-
-	stateType state = initState;
-	prevState = initState;
-
-	typedef rkDopri5<stateType> stepperType;
-
-	auto observer = [&](const stateType& state, double t){
-		// Do things...
+	auto wrappedSystem = [&](const stateType& state, stateType& ds, const double t){
+		takeStep(state, ds, path, loss, intensity);
 	};
 
-	integrate_const(make_dense_output(1.0e-6, 1.0e-6, stepperType()),
-			stochStep, state, 0.0, maxDepth, stepSize, observer);
+	boost::numeric::odeint::integrate_const(boost::numeric::odeint::make_dense_output(1.0e-6, 1.0e-6, stepperType()),
+						wrappedSystem, state, 0.0, 500.0, 0.5, observer);
+
+	return intensity;
+
+}
+
+void MonteCarlo::getUniformRandDirection(std::array<double, 3>& direction){
+
+	double theta = dist(gen) * M_PI, phi = dist(gen) * 2 * M_PI;
+
+	direction = {sin(theta) * cos(phi), pow(sin(theta), 2), cos(theta)};
+
+}
+
+void MonteCarlo::getSpecificIntensity(std::array<double, 3>& init, double& specificIntensity){
+
+	std::array<double, 3> direction;
+
+	for(int i = 0; i < numDirections; ++i){
+
+		getUniformRandDirection(direction);
+
+		for(int j = 0; j < numBundles; ++j)
+			specificIntensity += traceRay({init[0], init[1], init[2],
+					     direction[0], direction[1], direction[2]}) / numBundles;
+
+	}
+
+}
+
+void MonteCarlo::getSpectralDensity(std::array<double, 3> init){
+
+	std::vector<std::array<double, 2>> spectralDensity;
+
+	double specificIntensity;
+
+	while(true){
+
+		getSpecificIntensity(init, specificIntensity);
+		spectralDensity.push_back({specificIntensity, // Freq});
+
+	}
 
 }
