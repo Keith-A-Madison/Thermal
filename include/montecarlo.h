e7abab0f9578e8a410dd7069320cc8866c0a03e0
Keith Madison <96896521+Keith-A-Madison@users.noreply.github.com>
2023-07-19 18:09:14 -0400

    Update montecarlo.h

--- include/montecarlo.h
+++ include/montecarlo.h
@@ -1,25 +1,40 @@
 #include <boost/numeric/odeint.hpp>
 
 #ifndef montecarlo_H
-#define montecarlo_H
+#define montecarlo_G
 
 class MonteCarlo{
 
 public:
-	MonteCarlo();
-	void rayTrace(const stateType& initState);
+	using func1D = std::function<double(std::array<double, 3>&)>;
+	using func3D = std::function<std::array<double, 3>(std::array<double, 3>)>;
+
+	MonteCarlo(const func3D index, const func1D temp,
+			       const func1D atten, std::array<double, 8> bounds,
+			       int numDir, int numFreq, int numBundle);
+	void getSpectralDensity(std::array<double, 3> init);
 
 private:
-	void stochStep(const stateType& state, stateType& ds, const double /*t*/);
-	double uniformRand();
+	typedef std::array<double, 6> stateType; // Replace by using
 
-	typedef std::array<double, 6> stateType;
+	void takeStep(const stateType& state, stateType& ds, double& freePath, double& loss, double& intensity);
+	double traceRay(const stateType &init);
+	void getUniformRandDirection(std::array<double, 3>& dir);
+	void getSpecificIntensity(std::array<double, 3>& init, double& specificIntensity);
+	void phase(const stateType& state, stateType& ds);
 
 	stateType prevState;
 
-	std::uniform_real_distribution<double> dist(0.0, 1.0);
+	std::uniform_real_distribution<> dist;
 	std::random_device rd;
-	std::mt19937 gen; 
+	std::mt19937 gen;
+
+	func3D index;
+	func1D atten, temp;
+
+	std::array<double, 8> bounds;
+
+	int numFreq, numDir, numBundle;
 
 };
 
